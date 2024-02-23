421e04849aac775ac905413521ce849149b81117
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-23 09:39:09 +0000

    User Route added

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
+
 
 const app = express();
-<<<<<<< HEAD
 const port = 5000;
 
 app.use(bodyParser.json())
-=======
-const port = 3000;
->>>>>>> 96fb26e4d68477010e03856603d36bb446773442
+app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
     res.send("hello")
 })
 
-mongoose.connect('mongodb://localhost:27017/Crud', {
-    useNewurlparser: true,
-    useUnifiedtopology: true
-})
+// mongoose.connect('mongodb://localhost:27017/Crud', {
+//     useNewurlparser: true,
+//     useUnifiedtopology: true
+// })
 
-const peopleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true }, 
-    occupation: {type : String, required: true}
-});
-
-const People = mongoose.model('People', peopleSchema)
 
 app.listen(port, () => {
     console.log(`server is listening on ${port}`)
