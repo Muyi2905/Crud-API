caa4b147496b33feb86e5ac5fa3192e8291804e2
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-24 12:05:05 +0000

    defined schema

--- models/products.js
+++ models/products.js
@@ -1,8 +1,24 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const ProductSchema = mongoose.Schema({
-   name : {
-    type : String,
-    required : true
-   }
-})
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
